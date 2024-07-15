248e5a4f5e0b078bc01fd6c56d830cdd0646feb0
tacobayle <nicolas.nayle@gmail.com>
2024-07-15 14:02:09 +0200

    20240715_first-commit

--- apply.sh
+++ apply.sh
@@ -8,7 +8,7 @@ dc1_jump_private_key=$(terraform output -json | jq -r .jump_private_key.value)
 echo "jump IP Dc1 is: ${dc1_jump_ip}"
 echo "jump private key Dc1 is: ${dc1_jump_private_key}"
 #
-cd tfAviVcenterDc2
+cd ../tfAviVcenterDc2
 terraform init
 terraform apply -auto-approve -var-file=avi.json
 dc2_jump_ip=$(terraform output -json | jq -r .jump.value)
@@ -16,6 +16,7 @@ dc2_jump_private_key=$(terraform output -json | jq -r .jump_private_key.value)
 echo "jump IP Dc2 is: ${dc2_jump_ip}"
 echo "jump private key Dc2 is: ${dc2_jump_private_key}"
 #
+cd ..
 scp -o StrictHostKeyChecking=no -i ${dc2_jump_private_key} ubuntu@${dc2_jump_ip}:/home/ubuntu/avi_vcenter_yaml_values.yml ./dc2.yml
 scp -o StrictHostKeyChecking=no -i ${dc1_jump_private_key} ./dc2.yml ubuntu@${dc1_jump_ip}:/home/ubuntu/dc2.yml
 #
@@ -24,6 +25,3 @@ ssh -o StrictHostKeyChecking=no -i ${dc1_jump_private_key} -t ubuntu@${dc1_jump_
 ssh -o StrictHostKeyChecking=no -i ${dc1_jump_private_key} -t ubuntu@${dc1_jump_ip} "cd /home/ubuntu/alb-ui-api/backend/cert-ca ; /bin/bash cert-ca.sh "internal-private" true true"
 #
 ssh -o StrictHostKeyChecking=no -i ${dc1_jump_private_key} -t ubuntu@${dc1_jump_ip} "cd /home/ubuntu/alb-ui-api/backend/cert-app ; /bin/bash cert-app.sh "internal-private" true true"
-
-
-
